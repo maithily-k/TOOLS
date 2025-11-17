baf4f2a3646f434190ca0ad6b8151dc8dcc3ca28
kalub <kalub@Tarun>
2025-11-17 18:26:31 +0530

    Tl--added a comment

--- JUnitTestProj1/src/main/java/com/nt/service/Arithmetic.java
+++ JUnitTestProj1/src/main/java/com/nt/service/Arithmetic.java
@@ -2,7 +2,7 @@ package com.nt.service;
 
 public class Arithmetic {
     public float sum(float x,float y) {
-    	System.out.println("Sum is:");
+    	System.out.println("Sum is:");//prints sum
     	return x+y;
     }
 }
