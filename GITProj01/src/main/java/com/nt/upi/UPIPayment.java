5cd21166a6ef9681b9dae7df3b26c12c993c84af
kalub <kalub@Tarun>
2025-12-23 17:04:15 +0530

    UPI Payment completed by dev1

---
+++ GITProj01/src/main/java/com/nt/upi/UPIPayment.java
@@ -0,0 +1,7 @@
+package com.nt.upi;
+
+public class UPIPayment {
+ public String doUPIPayment(long mobileno,float amount) {
+	 return amount+" amount is transferred to mobile number : "+mobileno+" Using Bharatpay";
+ }
+}
